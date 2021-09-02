cef7282b452b314419ba87d6071cd394ea0c4be4
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2021-09-03 00:26:44 +0530

    List name update

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,20 @@
 
 <script>
 export default {
-  name: "Test",
+  name: "ShortenedList",
   data() {
     return {
       myList: [
-        "Item 1",
-        "Item 2",
-        "Item 3",
-        "Item 4",
-        "Item 5",
-        "Item 6",
-        "Item 7",
-        "Item 8",
-        "Item 9",
+        "Koenigsegg Jesko Absolut",
+        "Bugatti Chiron Super Sport",
+        "SSC Tuatara",
+        "Hennessey Venom F5",
+        "Aston Martin Valkyrie",
+        "McLaren Speedtail",
+        "Koenigsegg Gemera",
+        "Pagani Huayra BC Roadster",
+        "Rimac Concept_One",
+        "Lamborghini Sian Roadster",
       ],
     };
   },
