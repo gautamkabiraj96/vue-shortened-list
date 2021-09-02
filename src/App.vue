fecb07c5c00126fe08c96d2f01222f8906205cf5
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2021-09-03 00:20:52 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in getShortList(5)" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      myList: [
+        "Item 1",
+        "Item 2",
+        "Item 3",
+        "Item 4",
+        "Item 5",
+        "Item 6",
+        "Item 7",
+        "Item 8",
+        "Item 9",
+      ],
+    };
+  },
+  methods: {
+    getShortList(shortListSize) {
+      return this.myList.slice(0, shortListSize);
+    },
+  },
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in myList" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      myList: [
+        "Item 1",
+        "Item 2",
+        "Item 3",
+        "Item 4",
+        "Item 5",
+        "Item 6",
+        "Item 7",
+        "Item 8",
+        "Item 9",
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
